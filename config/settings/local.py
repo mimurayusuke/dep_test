5c3eab7e72a9da794f2663175937c523ec44a1c2
Yusuke Mimura <mimurayusuke0406@gmail.com>
2020-06-07 18:55:06 +0900

    first commit

---
+++ config/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *
+
+SECRET_KEY = '0=qt9!wc&msa%oqq-vg@bug!lpf!)e2!j%y7&v(28d9d2(9o27'
+
+DEBUG = True
+
+ALLOWED_HOST = []
+
+STATIC_URL = '/static/'
+#STATICFILES_DIRSをコメントアウトしても、ローカル環境では問題なく動作した。
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
+#開発環境でcollectstaticはしないので必要ないかもしれない
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ config/settings/production.py
@@ -0,0 +1,17 @@
+from .base import *
+
+SECRET_KEY = '0=qt9!wc&msa%oqq-vg@bug!lpf!)e2!j%y7&v(28d9d2(9o27'
+
+DEBUG = False
+
+ALLOWED_HOST = ['yskexe.com']
+
+STATIC_URL = '/static/'
+#本番環境なので、STATICFILES_DIRSは必要ないはず
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#本番環境で利用するWebサーバ nginxのディレクトリに静的ファイルを集める。
+STATIC_ROOT = '/usr/share/nginx/html/static'
+
+#本番環境で利用するWebサーバ nginxのディレクトリにメディアファイルを集める。
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/usr/share/nginx/html/media'
