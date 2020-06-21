5dcbca6da95f434d159ad00130e7b042723e2a64
Yusuke Mimura <mimurayusuke0406@gmail.com>
2020-06-21 17:51:18 +0900

    add base.html

--- weight/models.py
+++ weight/models.py
@@ -5,7 +5,7 @@ class Menu(models.Model):
     menu_name = models.CharField(max_length=30)
 
     def __str__(self):
-        return "{0}:{1}".format(self.pk, self.menu_name)
+        return "PK:{0}, name:{1}".format(self.pk, self.menu_name)
 
 class Record(models.Model):
     weight_menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
@@ -14,4 +14,4 @@ class Record(models.Model):
     modified_at = models.DateField(auto_now=True)
 
     def __str__(self):
-        return "{0}:{1}:{2}".format(self.pk, self.weight_menu, self.weight_record)
+        return "PK:{0}, FK:{1}, weight:{2}, created:{3}".format(self.pk, self.weight_menu, self.weight_record, self.created_at)
