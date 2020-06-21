592c1bf893c264a05428ec6cfa03a058a8188a0c
Yusuke Mimura <mimurayusuke0406@gmail.com>
2020-06-21 15:40:46 +0900

    add menu model

--- weight/models.py
+++ weight/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    weight_menu = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "{0}:{1}".format(self.pk, self.weight_menu)
+    
